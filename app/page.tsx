a804d67728223b54191d2b87ced75fe0b3fd6233
Eduardo Cobuci <educobuci@gmail.com>
2024-10-12 17:08:09 -0300

    Setup shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,7 @@ import Users from '@/models/users';
 export default async function Home() {
   const users = await Users.all.select().toArray();
 
-  return <div className="flex p-8">Hello, next { users.length }!</div>;
+  return <div className="flex flex-col items-start p-8">
+    Hello, next { users.length }!
+  </div>;
 }
