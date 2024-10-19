38c4fff48623bfb013c0fe13dda1e34f12ada735
Eduardo Cobuci <educobuci@gmail.com>
2024-10-19 10:59:05 -0300

    Adjust layout

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Page({ loadTodoList }: ITodoListProps) {
   const todoList = await loadTodoList(1)
 
   return (
-    <main className="flex flex-col items-center justify-center md:h-screen space-y-8">
+    <main className="flex flex-col items-center justify-center h-screen space-y-8 bg-slate-100">
       <TodoList todoList={todoList} />
     </main>
   )
--- components/todoList.tsx
+++ components/todoList.tsx
@@ -5,52 +5,40 @@ import { useState } from 'react'
 import { Input } from '@/components/ui/input'
 import { Button } from './ui/button'
 import { TodoList } from '@/domain/entities/TodoList'
+import { Card, CardContent, CardHeader } from './ui/card'
 
 interface TodoListProps {
   todoList: TodoList
 }
 
 export default function ({ todoList }: TodoListProps) {
-  // const [items, setItems] = useState(todoItems)
-  // const [newItem, setNewItem] = useState('')
-
-  // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault()
-  //   setItems([
-  //     ...items,
-  //     { id: items.length + 1, title: newItem, isCompleted: false },
-  //   ])
-  //   setNewItem('')
-  // }
-
   return (
-    <div className="flex flex-col space-y-8">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
-        TODO
-      </h1>
-      {/* <form onSubmit={handleSubmit} className="flex gap-2">
-        <Input
-          autoFocus
-          type="text"
-          placeholder="Add new todo"
-          onChange={(e) => setNewItem(e.target.value)}
-          value={newItem}
-        />
-        <Button type="submit">Add</Button>
-      </form> */}
-      <ul>
-        {todoList.items.map((item, index) => (
-          <li key={index} className={`${item.isCompleted && 'line-through'}`}>
-            <input
-              id={`${index}`}
-              type="checkbox"
-              checked={item.isCompleted}
-              onChange={() => {}}
-            />
-            <label htmlFor={`${index}`}> {item.title}</label>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Card className="w-full max-w-96">
+      <CardHeader>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
+          TODO
+        </h1>
+      </CardHeader>
+      <CardContent>
+        <ul>
+          {todoList.items.map((item, index) => (
+            <li
+              key={index}
+              className={`${
+                item.isCompleted && 'line-through'
+              } border-t border-slate-200 p-2`}
+            >
+              <input
+                id={`${index}`}
+                type="checkbox"
+                checked={item.isCompleted}
+                onChange={() => {}}
+              />
+              <label htmlFor={`${index}`}> {item.title}</label>
+            </li>
+          ))}
+        </ul>
+      </CardContent>
+    </Card>
   )
 }
